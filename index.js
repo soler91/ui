c1c9e9eeba80ab309e5c573a1d074b5c15b4d8a4
Pinkie Pie <yarnovax@gmail.com>
2018-03-20 22:23:09 -0400

    Fix race condition

--- index.js
+++ index.js
@@ -3,27 +3,32 @@
 const Express = require('express'),
 	{host, port} = require('./config')
 
-let serverPort = 0,
+let serverQ = null,
+	serverPort = 0,
 	activeRouter = null
 
 async function getServer(router) {
 	activeRouter = router
 
 	if(!serverPort) // Lazy-load
-		serverPort = await new Promise((resolve, reject) => {
-			const app = Express()
-			.set('env', 'production')
-			.enable('case sensitive routing')
-			.disable('x-powered-by')
-			.use((req, res, next) => { activeRouter(req, res, next) })
-			.use((req, res) => { res.status(404).end() })
-			.use((err, req, res, next) => {
-				console.error(err)
-				res.status(500).end()
-			})
-			.listen(port, host, () => { resolve(app.address().port) })
-			.on('error', reject)
-		})
+		if(!serverQ) {
+			serverPort = await (serverQ = new Promise((resolve, reject) => {
+				const app = Express()
+				.set('env', 'production')
+				.enable('case sensitive routing')
+				.disable('x-powered-by')
+				.use((req, res, next) => { activeRouter(req, res, next) })
+				.use((req, res) => { res.status(404).end() })
+				.use((err, req, res, next) => {
+					console.error(err)
+					res.status(500).end()
+				})
+				.listen(port, host, () => { resolve(app.address().port) })
+				.on('error', reject)
+			}))
+			serverQ = null
+		}
+		else await serverQ
 
 	return `${host}:${serverPort}`
 }
